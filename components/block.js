2866102fa89928c78d091c786a4a832fd5694fbb
Nicholas Smith <nick@polarity.io>
2021-07-27 21:19:50 -0700

    sumo logic components

--- components/block.js
+++ components/block.js
@@ -1,4 +1,19 @@
 polarity.export = PolarityComponent.extend({
-    details: Ember.computed.alias('block.data.details'),
-    entity: Ember.computed.alias('block.entity'),
+  details: Ember.computed.alias('block.data.details'),
+  activeTab: 'displayMessages',
+  actions: {
+    changeTab: function (tabName) {
+      this.set('activeTab', tabName);
+    },
+    // makeRequest: function () {
+    //   const payload = {
+    //     type: 'makeRequest', 
+    //     action: 'Button Clicked'
+    //   }
+
+    //   this.sendIntegrationMessage(payload).then((res) => {
+    //     this.set("")
+    //   })
+    // }
+  }
 });
