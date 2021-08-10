468c7efdcac03f4e22c5cd5ce609862f553fc2bd
Nicholas Smith <nick@polarity.io>
2021-08-09 17:41:25 -0700

    add error handler

--- utils/errorToPojo.js
+++ utils/errorToPojo.js
@@ -1,13 +1,43 @@
-const errorToPojo = (detail, err) => {
-  return err instanceof Error
-    ? {
-        ...err,
-        name: err.name,
-        message: err.message,
-        stack: err.stack,
-        detail: detail ? detail : 'Unexpected error encountered'
-      }
-    : err;
-};
+const { GaxiosError } = require('gaxios');
 
-module.exports = errorToPojo;
+function gaxiosErrorToPojo(err) {
+  if (err instanceof GaxiosError) {
+    let detail = 'There was an unexpected error';
+    let data = '';
+    if (err.response && err.response.status === 503) {
+      detail = `Lookup limit reached.  Please wait a moment and try searching fewer indicators.`;
+      data = err.response.data;
+    } else if (err.response) {
+      detail = `Received unexpected HTTP status ${err.response.status}`;
+      data = err.response.data;
+    } else if (err.request) {
+      detail = `There was an HTTP error`;
+    } else {
+      detail = err.message;
+    }
+    return {
+      name: err.name,
+      message: err.message,
+      request: err.request,
+      data,
+      detail
+    };
+  } else if (err instanceof Error) {
+    // Handle Node Errors as well as FetchError
+    // See https://github.com/node-fetch/node-fetch/blob/master/docs/ERROR-HANDLING.md for information
+    // on Node Fetch error structure
+    return {
+      name: err.name,
+      message: err.message,
+      stack: err.stack,
+      code: err.code,
+      errno: err.errno,
+      type: err.type,
+      detail: err.message ? err.message : 'There was an unexpected error'
+    };
+  } else {
+    return err;
+  }
+}
+
+module.exports = gaxiosErrorToPojo;
